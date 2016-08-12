9ca7542ea83497cfa9103aed95e4e6d7f125a888
vodin <vodin@61a8709e-89bf-4eb9-bdc2-e47853f02301>
2016-08-12 15:03:16 +0000

    clearup

--- code/client/cl_curl.cpp
+++ code/client/cl_curl.cpp
@@ -2,20 +2,20 @@
 ===========================================================================
 Copyright (C) 2006 Tony J. White (tjw@tjw.org)
 
-This file is part of Quake III Arena source code.
+This file is part of Qio source code.
 
-Quake III Arena source code is free software; you can redistribute it
+Qio source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of the License,
 or (at your option) any later version.
 
-Quake III Arena source code is distributed in the hope that it will be
+Qio source code is distributed in the hope that it will be
 useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Quake III Arena source code; if not, write to the Free Software
+along with Qio source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
--- game/baseqio/srcItemDefs/quake3Items.c
+++ game/baseqio/srcItemDefs/quake3Items.c
@@ -2,14 +2,14 @@
 ===========================================================================
 Copyright (C) 1999-2005 Id Software, Inc.
 
-This file is part of Quake III Arena source code.
+This file is part of Qio source code.
 
-Quake III Arena source code is free software; you can redistribute it
+Qio source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of the License,
 or (at your option) any later version.
 
-Quake III Arena source code is distributed in the hope that it will be
+Qio source code is distributed in the hope that it will be
 useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
